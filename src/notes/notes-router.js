f4ec26b1128f7e18e6e06db41497aed693797b5b
chrisli5 <chrisli41@gmail.com>
2019-12-02 14:16:15 -0800

    Fix notes endpoint

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -12,7 +12,7 @@ const serializeNote = note => ({
     name: xss(note.name),
     content: xss(note.content),
     modified: xss(note.modified),
-    folderId: note.folderId,
+    folderId: note.folderid,
 })
 
 notesRouter
@@ -28,9 +28,9 @@ notesRouter
 
     .post(bodyParser, (req, res, next) => {
         const { name, content, folderId } = req.body;
-        const newNote = { name, content, folderId };
+        const newNote = { name, content, folderid: folderId };
 
-        for (const field of ['name', 'content', 'folderId']) {
+        for (const field of ['name', 'content', 'folderid']) {
             if (!newNote[field]) {
                 logger.error(`${field} is required.`)
                 return res.status(404).send({
@@ -54,53 +54,53 @@ notesRouter
     })
 
 notesRouter
-    .route('/:folderId')
+    .route('/:noteId')
     .all((req, res, next) => {
-        const { folderId } = req.params
-        NotesService.getById(req.app.get('db'), folderId)
-            .then(folder => {
-                if(!folder) {
-                    logger.error(`Folder with id ${folderId} not found.`)
+        const { noteId } = req.params
+        NotesService.getById(req.app.get('db'), noteId)
+            .then(note => {
+                if(!note) {
+                    logger.error(`Note with id ${noteId} not found.`)
                     return res.status(404).json({
-                        error: { message: `Folder not found.`}
+                        error: { message: `Note not found.`}
                     })
                 }
 
-                res.folder = folder;
+                res.note = note;
                 next();
             })
             .catch(next)
     })
 
     .get((req, res) => {
-        res.json(serializeNote(res.folder));
+        res.json(serializeNote(res.note));
     })
 
     .delete((req, res, next) => {
-        const { folderId } = req.params;
-        NotesService.deleteFolder(req.app.get('db'), folderId)
+        const { noteId } = req.params;
+        NotesService.deleteNote(req.app.get('db'), noteId)
             .then(numRowsAffected => {
-                logger.info(`Folder with id ${folderId} deleted.`)
+                logger.info(`Note with id ${noteId} deleted.`)
                 res.status(204).end();
             })
             .catch(next)
     })
 
     .patch(bodyParser, (req, res, next) => {
-        const { name } = req.body;
-        if (!name) {
-            logger.error('Invalid update without name field.')
+        const { name, content, folderId } = req.body;
+        if (!name || !content || !folderId) {
+            logger.error('Invalid update without required fields.')
             return res.status(400).json({
                 error: {
-                    message: `Request body must contain name`
+                    message: `Request body must contain name, content, folderId`
                 }
             })
         }
 
-        NotesService.updateFolder(
+        NotesService.updateNote(
             req.app.get('db'),
-            req.params.folderId,
-            { name }
+            req.params.noteId,
+            { name, content, folderid: folderId }
         )
             .then(numRowsAffected => {
                 res.status(204).end()
